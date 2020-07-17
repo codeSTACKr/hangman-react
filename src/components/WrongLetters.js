867645b6a0225540e3f6b876e84078afbeb62e3f
Jesse Hall <you@example.com>
2020-07-17 15:00:27 -0500

    fix wrong letter rendering

--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -4,10 +4,14 @@ const WrongLetters = ({ wrongLetters }) => {
 
   return (
     <div className="wrong-letters-container">
-      {wrongLetters.length > 0 && 
-        <p>Wrong</p>
-      }
-      {wrongLetters.map((letter, i) => <span key={i}>{letter}</span>)}
+      <div>
+        {wrongLetters.length > 0 && 
+          <p>Wrong</p>
+        }
+        {wrongLetters
+          .map((letter, i) => <span key={i}>{letter}</span>)
+          .reduce((prev, curr) => prev === null ? [curr] : [prev, ', ', curr], null)}
+      </div>
     </div>
   )
 }
